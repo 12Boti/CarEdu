305f624c98d2514f815d8d2d3081a8ff867dcb51
Olle Boti <olle12boti+git@gmail.com>
2022-05-24 19:01:13 +0200

    offset boxes randomly

--- Boxes.gd
+++ Boxes.gd
@@ -6,6 +6,7 @@ var _text: String
 func _ready():
 	(get_node("Sprite3D") as Sprite3D).texture = (get_node("Sprite3D/Viewport") as Viewport).get_texture()
 	(get_node("Sprite3D/Viewport/Label") as Label).text = _text
+	translate(Vector3(randf() * 2 - 1, 0, 0) * 2)
 
 func set_text(t: String):
 	_text = t
